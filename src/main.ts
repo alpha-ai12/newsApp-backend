aedc6de5be2efa8b8b83cfb50c77875fca9dcb88
CodeSourabh <sourabh.gupta@ultra.me>
2024-08-17 17:33:54 +0530

    update the cors configurations

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,19 @@ import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.use(compression({
-    filter: () => { return true },
-    threshold: 0
+  app.enableCors({
+    origin: '*', // Replace '*' with your specific frontend domain for better security
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+  app.use(
+    compression({
+      filter: () => {
+        return true;
+      },
+      threshold: 0,
   }));
   await app.listen(3004);
 }
